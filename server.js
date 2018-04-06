b7adea73d600a1a347a01a6ecd2eab54b048a63f
Simon Norman <01secn@gmail.com>
2018-04-06 15:28:36 +0100

    Initial app creation

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static(path.join(__dirname)));
+app.use("/styles", express.static(__dirname));
+app.use("/images", express.static(__dirname + '/images'));
+app.use("/scripts", express.static(__dirname + '/scripts'));
+
+// viewed at based directory http://localhost:8080/
+app.get('/', function (req, res) {
+  res.send("Hello world!");
+});
+
+app.listen(process.env.PORT || 8080);

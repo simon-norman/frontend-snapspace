4b445b7a947d49044c919cd4d27cfb5861bd0f19
Simon Norman <01secn@gmail.com>
2018-04-16 13:11:21 +0100

    Adding send file options

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const sendFileOptions = {
+  root: __dirname
+}
 
 app.use(express.static(path.join(__dirname)));
 
 // viewed at based directory http://localhost:8080/
 app.get('/', function (req, res) {
-  res.sendFile('./views/snapshot_view.html');
+  res.sendFile('./views/snapshot_view.html', sendFileOptions);
 });
 
 app.listen(process.env.PORT || 8080);

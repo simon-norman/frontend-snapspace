24408b5f04fd7a0f484e87c9c6cae77692dd7f81
Simon Norman <01secn@gmail.com>
2018-04-16 11:50:02 +0100

    Added mocha, chai, and initial sense check Mocha suite

---
+++ test/main_test.js
@@ -0,0 +1,7 @@
+const expect = require('chai').expect;
+
+describe('Mocha sense check', function () {
+    it('Should run our tests using npm', function () {
+        expect(true).to.be.ok;
+    });
+});

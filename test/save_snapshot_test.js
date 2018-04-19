6dd42255c2b0bbfb983b6f99ceeb3be69134c66e
Simon Norman <01secn@gmail.com>
2018-04-19 10:09:08 +0100

    New save snapshot test file added

--- test/save_snapshot_test.js
+++ test/save_snapshot_test.js
@@ -1,5 +1,10 @@
 
+var expect = require('chai').expect;
+
 describe('convertImageTo', () => {
+    const saveImage = require('../controllers/snapshot_controller').saveImage;
+
     it('it should save the image to file storage', (done) => {
+
     });
 });

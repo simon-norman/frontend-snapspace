43ef85168eccffe52cf171099b1224a3f43ccc2b
Simon Norman <01secn@gmail.com>
2018-04-19 13:11:09 +0100

    added basic config file

---
+++ app/config.js
@@ -0,0 +1,21 @@
+// config.js
+const env = process.env.NODE_ENV; // 'dev' or 'test'
+
+const dev = {
+ app: {
+    port: parseInt(process.env.DEV_APP_PORT) || 3000
+ }
+};
+
+const test = {
+ app: {
+   port: parseInt(process.env.TEST_APP_PORT) || 3000
+ }
+};
+
+const config = {
+ dev,
+ test
+};
+
+module.exports = config[env];

8710f1f1bf99e78c5159b95b580578fe2069988b
Simon Norman <01secn@gmail.com>
2018-04-19 10:09:44 +0100

    chai added

--- test/save_snapshot_test.js
+++ test/save_snapshot_test.js
@@ -1,5 +1,5 @@
 
-var expect = require('chai').expect;
+const expect = require('chai').expect;
 
 describe('convertImageTo', () => {
     const saveImage = require('../controllers/snapshot_controller').saveImage;

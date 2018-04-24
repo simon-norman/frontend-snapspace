ae9b07e505b298008433de5d75696a0a4556651e
Simon Norman <01secn@gmail.com>
2018-04-24 16:36:35 +0100

    Upload UI updated with comment field and submit button

--- app/snapshot_app.js
+++ app/snapshot_app.js
@@ -3,15 +3,17 @@ var snapshotModule = ( function() {
     'use strict';
     
     var snapshot = {
-        "imageURL": '',
-        "comment": '',
+        imageURL: '',
+        comment: ''
     };
 
     var image = '';
 
-    function saveSnapshot(image) {
+    function saveSnapshot(image, comment) {
         snapshotModuleAPI.saveImage(image, (imageURL) => {
             snapshot.imageURL = imageURL;
+            snapshot.comment = comment;
+            console.log(snapshot);
             snapshotModuleAPI.saveSnapshot(snapshot,
                 () => {
                     //placeholder to handle response
@@ -21,7 +23,11 @@ var snapshotModule = ( function() {
 
     document.getElementById('addphoto').addEventListener('change', function () {
         image = this.files[0];
-        saveSnapshot(image);
+    });
+
+    document.getElementById('submit').addEventListener('click', function () {
+        var comment = document.getElementById("snapshotcomment").value
+        saveSnapshot(image, comment);
     });
 
 }());
--- server.js
+++ server.js
@@ -2,30 +2,31 @@
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').load();
 }
+
 const express = require('express'),
-      app = express(),
-      mongoose = require('mongoose'),
-      routes = require('./routes');
-      bodyParser = require('body-parser'),
-      config = require("./config.js").get(process.env.NODE_ENV),
-      path = require('path'),
-      snapshotController = require
-      ("./controllers/snapshot_controller.js");
+  app = express(),
+  mongoose = require('mongoose'),
+  routes = require('./routes'),
+  bodyParser = require('body-parser'),
+  config = require("./config.js").get(process.env.NODE_ENV),
+  path = require('path'),
+  snapshotController = require
+    ("./controllers/snapshot_controller.js");
 
 mongoose.connect(config.database.uri);
 mongoose.connection
-      .once('open', () => { console.log("DB connection complete") })
-      .on('error', (error) => {
-          console.log(error);
-      });
+  .once('open', () => { console.log("DB connection complete") })
+  .on('error', (error) => {
+    console.log(error);
+  });
+
+app.use(bodyParser.json({ limit: '50mb' }));
+
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 
 app.use('/', routes);
 
 app.use(express.static(path.join(__dirname)));
-
-app.use(bodyParser.json({limit: '50mb'}));
-
-app.use(bodyParser.urlencoded({extended: true, limit: '50mb'}));
 
 app.listen(process.env.PORT || config.app.port);
 

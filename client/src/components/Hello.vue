1461018e9a2feaf3c807b94dfc23173b7195a07e
Simon Norman <01secn@gmail.com>
2018-05-01 10:09:12 +0100

    Playing around with Vue.js instance

--- client/src/components/Hello.vue
+++ client/src/components/Hello.vue
@@ -1,38 +1,39 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="http://chat.vuejs.org/" target="_blank" rel="noopener">Vue Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank" rel="noopener">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div>
+      <p id="photomessage"></p>
+      <label>Take photo
+        <input type="file" id="addphoto" accept="image/*" style="display: none;">
+      </label>
+        <div>
+          <div>
+            <span>Tell us more</span>
+          </div>
+          <textarea id="snapshotcomment"></textarea>
+        </div>
+        <button id="submit">Submit</button>
+      </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'hello',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js PWA'
-    }
+  name: '',
+  data: {
+    image: '',
+    comment: ''
+  },
+  takePhoto(event) {
+    this.image = event.files[0];
+    console.log(this.image);
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 
@@ -47,6 +48,6 @@ li {
 }
 
 a {
-  color: #35495E;
+  color: #35495e;
 }
 </style>

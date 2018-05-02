738a4d395be679f8f4e9e8567ebc95c69c63a440
Simon Norman <01secn@gmail.com>
2018-05-02 19:07:24 +0100

    Removed dist from gitignore so can actually be served from Heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express'),
 
 const app = express();
 
-app.use("/", serveStatic ( path.join (__dirname, 'dist') ) );
+app.use("/", serveStatic ( path.join (__dirname, '/dist') ) );
 
 const port = process.env.PORT || 5000
 app.listen(port)

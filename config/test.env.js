cffedd5a085c9a35aba08fa4da1928d200af0c66
Simon Norman <01secn@gmail.com>
2018-05-09 13:54:15 +0100

    Testing removing test environment variables to see if can just run from Heroku

--- config/test.env.js
+++ config/test.env.js
@@ -1,9 +1,2 @@
 'use strict'
 
-const merge = require('webpack-merge')
-const devEnv = require('./dev.env')
-
-module.exports = merge(devEnv, {
-  NODE_ENV: '"test"',
-  SNAPSPACE_API: JSON.stringify(process.env.SNAPSPACE_API)
-})

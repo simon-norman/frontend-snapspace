fef3ef29802b36cfdc1c2135b4d5f88ef4445eb8
Simon Norman <01secn@gmail.com>
2018-05-24 14:15:03 +0100

    Updated API

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default class Api {
-  constructor(baseURL) {
+  constructor({ baseURL = '' }) {
     this.axios = axios.create({
       baseURL,
       responseType: 'json',
--- src/api/imageApi.js
+++ src/api/imageApi.js
@@ -1,6 +1,9 @@
 import Api from './Api';
 
 export default class ImageApi extends Api {
+  constructor() {
+    super({ baseURL: '' });
+  }
   putImage(URL, imageFile, params) {
     return this.axios.put(URL, imageFile, params);
   }

2230bb9ea85bb89396244d76fedb89378ca04785
Simon Norman <01secn@gmail.com>
2018-07-24 16:40:11 +0100

    Updated production webpack config to generate CSS without hash, thereby enabling it to get up to date CSS when new CSS file deployed

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -45,7 +45,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     }),
     // extract css into its own file
     new ExtractTextPlugin({
-      filename: utils.assetsPath('css/[name].[contenthash].css')
+      filename: utils.assetsPath('css/[name].css')
     }),
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
